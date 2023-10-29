279eefa5c7033ae4aaf2aa9a9c6e59cfc9339c1e
Opstic <46141527+opstic@users.noreply.github.com>
2023-10-29 12:53:38 -0400

    Remove notice as installation is no longer needed

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,3 @@
-// prerequisites: Install Turbo-Base64 library tb64lib under /usr/lib or /usr/local/lib  
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
